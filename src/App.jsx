ab3ad2656f9b08bfcb431cee5b4d2d4b7a448af0
Andry Narson <119520276+rbd3@users.noreply.github.com>
2024-07-03 08:28:58 +0000

    Update about info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "RABEDESANA Andry Narson",
+  title: "Software Developer",
+  email: "rabedesanaandry01@gmail.com",
+  gitHub: "https://github.com/rbd3",
+  linkedIn: "https://www.linkedin.com/in/andry-rabedesana/",
 };
 
 const primaryColor = "#4E567E";
